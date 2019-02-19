e63f8faa9e0787d80a9ef5378e01fe9f2348c88c
DinhQuangBang <dinhquangbangnd97@gmail.com>
2019-02-19 22:08:20 +0700

    initial commit

---
+++ Seasion1/turtle_intro.py
@@ -0,0 +1,38 @@
+from turtle import *
+speed(0)
+shape("turtle")
+
+# for i in range(200):
+#     for j in range(4):
+#         forward(100)
+#         left(90)
+#     right(50)
+#   forward(100)
+#   left(90)
+#   forward(100)
+#   left(90)
+#   forward(100)
+
+# color("red", "yellow")
+
+# begin_fill()
+# for i in range (4):
+#     forward(100)
+#     left(90)
+# end_fill()
+
+
+# for i in range (4):
+#     forward(50)
+#     penup()
+#     forward(50)
+#     pendown()
+#     left(90)
+
+circle (100)
+
+
+
+   
+
+mainloop()
